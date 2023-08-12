5cdd70ec1c92aacc0f815f7505b341b202527f81
martin-b-07e5 <martin.bergagno@gmail.com>
2023-08-12 02:20:23 -0300

    api_key now works in pythonanywhere

--- api_key_utils.py
+++ api_key_utils.py
@@ -1,11 +1,17 @@
 import configparser
+import pathlib
 
 
 def api_key_utils():
+
+    # https://www.pythonanywhere.com/forums/topic/30771/#id_post_100134
+    config_path = pathlib.Path(__file__).parent.absolute() / "config.ini"
+    
     # Crea un objeto ConfigParser
     config = configparser.ConfigParser()
     # Lee el archivo de configuración
-    config.read('config.ini')
+    # config.read('config.ini')
+    config.read(config_path)
 
     # Obtén los valores de latitud, longitud y clave API del archivo de configuración config.ini
     # lat = config.getfloat('WeatherAPI', 'lat')
